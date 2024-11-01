4ba114a6e7b5790b3290ddd1cb0c5e5acbd4bdc0
Peter Adams <pp_adams@apple.com>
2024-11-01 13:51:32 +0000

    Fix license header

--- scripts/gha_run_unit_tests.sh
+++ scripts/gha_run_unit_tests.sh
@@ -3,11 +3,11 @@
 ##
 ## This source file is part of the Swift Cassandra Client open source project
 ##
-## Copyright (c) 2017-2018 Apple Inc. and the SwiftNIO project authors
+## Copyright (c) 2017-2018 Apple Inc. and the Swift Cassandra Client project authors
 ## Licensed under Apache License v2.0
 ##
 ## See LICENSE.txt for license information
-## See CONTRIBUTORS.txt for the list of SwiftNIO project authors
+## See CONTRIBUTORS.txt for the list of Swift Cassandra Client project authors
 ##
 ## SPDX-License-Identifier: Apache-2.0
 ##
