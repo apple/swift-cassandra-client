d2945e2f621b5c8e5c6fefcf59e26df9d4a30032
Igor Milyakov <imilyakov@apple.com>
2025-12-09 15:48:09 -0800

    Changing type to Int
    (cherry picked from commit d183c0ec6faa61bb9ee942eec725f23fc394963c)

--- Sources/CassandraClient/Session.swift
+++ Sources/CassandraClient/Session.swift
@@ -1067,7 +1067,7 @@ extension CassandraClient {
             let eventLoop = eventLoop ?? self.eventLoopGroup.next()
 
             do {
-                try statement.setPagingSize(pageSize)
+                try statement.setPagingSize(Int(pageSize))
             } catch {
                 if let cassError = error as? CassandraClient.Error {
                     CassandraClient.RequestLog.logFailure(
@@ -1902,7 +1902,7 @@ extension CassandraClient.Session {
         logger: Logger? = .none
     ) async throws -> CassandraClient.PaginatedRows {
         do {
-            try statement.setPagingSize(pageSize)
+            try statement.setPagingSize(Int(pageSize))
         } catch {
             if let cassError = error as? CassandraClient.Error {
                 CassandraClient.RequestLog.logFailure(
